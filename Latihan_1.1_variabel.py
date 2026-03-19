9036872161e07a49329ff8f4b57f19cc839e1305
Leonardo yanuarianto <leonardoyanuarianto907@gmail.com>
2026-03-20 00:31:46 +0700

    Add Latihan_1.1_variabel.py file

---
+++ Latihan_1.1_variabel.py
@@ -0,0 +1,23 @@
+# python_learning
+nama = "Marsha"
+#contoh variabel tipe string
+umur = 15 
+#contoh variabel tipe integer
+berat = 52.3 
+#contoh variabwl tipe float
+ngarang = True 
+#contoh variabel tipe boolean
+
+print(type(nama)) 
+print(type(umur)) 
+print(type(berat))
+print(type(ngarang))
+
+#nama, umur, berat, ngarang adalah contoh dari variabel
+#print adalah sintaks untuk memunculkan output atau teks
+#sintaks (type) digunakan untuk melihat tipe data suatu variabel
+
+print("Nama adik saya adalah ", nama)
+print("Dia saat ini berusia", umur, "tahun")
+print("Bb dia", berat, "Kg")
+print("Apakah anda ngarang?", ngarang)
