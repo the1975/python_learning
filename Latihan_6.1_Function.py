377383a35c4278000f31efdbd5d7e0f02ae2a397
Leon <leonardoyanuarianto907@gmail.com>
2026-09-26 16:10:07 +0700

    update Latihan_6.1_Function.py

---
+++ Latihan_6.1_Function.py
@@ -0,0 +1,9 @@
+# Deskripsi: Program dengan implementasi Function tanpa Parameter dan tanpa Return.
+
+def sapa_pengguna():
+    # Function ini hanya mencetak teks, tidak menerima input dan tidak mengembalikan hasil.
+    print("Halo! Saya Baedo.")
+    print("Boleh Kita Berkenalan!")
+
+# Memanggil function agar kodenya dieksekusi
+sapa_pengguna()
